c46ea297c905175206bec87db4ecdd9951b3ebb9
abdishfudhf2-cell <abdishfudhf2@gmail.com>
2026-06-06 23:52:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,11 @@ def dashboard():
 
     return render_template("dashboard.html", result=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        debug=True
+    )
